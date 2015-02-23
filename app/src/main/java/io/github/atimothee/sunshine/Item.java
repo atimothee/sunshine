7afef7ab33716950f7febca3b0fd361a110f5b11
Timothy Asiimwe <atimothee@gmail.com>
2015-02-23 13:20:42 +0300

    class version, buggy

---
+++ app/src/main/java/io/github/atimothee/sunshine/Item.java
@@ -0,0 +1,10 @@
+package io.github.atimothee.sunshine;
+
+import java.util.List;
+
+/**
+ * Created by Timo on 2/21/15.
+ */
+public class Item {
+    public List<Recipe> items;
+}
---
+++ app/src/main/java/io/github/atimothee/sunshine/Recipe.java
@@ -0,0 +1,15 @@
+package io.github.atimothee.sunshine;
+
+/**
+ * Created by Timo on 2/21/15.
+ */
+public class Recipe {
+    public Long id;
+    public String description;
+    public String ingredients;
+    public String directions;
+    public String image_url;
+    public String kind;
+    public Long categoryId;
+
+}
