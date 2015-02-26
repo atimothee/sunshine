9a543cca47a85da66bfb392a453413317aff1efc
Timothy Asiimwe <atimothee@gmail.com>
2015-02-27 01:21:51 +0300

    added show pref location on map

--- app/src/main/java/io/github/atimothee/sunshine/MainActivity.java
+++ app/src/main/java/io/github/atimothee/sunshine/MainActivity.java
@@ -1,13 +1,18 @@
 package io.github.atimothee.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+    static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +46,28 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
+        else if(id == R.id.action_map){
+            openPreferredLocationInMap();
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void openPreferredLocationInMap(){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter("q", location)
+                .build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
+        else {
+            Log.d(LOG_TAG, "Couldn't call "+location+", no suitable app installed");
+        }
+
     }
 
 }
