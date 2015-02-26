e643757959ca38da996b134f486abb3e8704edee
Timothy Asiimwe <atimothee@gmail.com>
2015-02-27 00:33:59 +0300

    added settings activity without gist code

--- app/src/main/java/io/github/atimothee/sunshine/DetailActivity.java
+++ app/src/main/java/io/github/atimothee/sunshine/DetailActivity.java
@@ -44,6 +44,7 @@ public class DetailActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
---
+++ app/src/main/java/io/github/atimothee/sunshine/SettingsActivity.java
@@ -0,0 +1,9 @@
+package io.github.atimothee.sunshine;
+
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by Timo on 2/27/15.
+ */
+public class SettingsActivity extends PreferenceActivity{
+}
