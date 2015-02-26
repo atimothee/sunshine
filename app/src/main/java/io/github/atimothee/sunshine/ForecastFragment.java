20a292eaf0746435fee1dab83421bf9ba525001c
Timothy Asiimwe <atimothee@gmail.com>
2015-02-27 00:51:58 +0300

    refresh fetching location from settings

--- app/src/main/java/io/github/atimothee/sunshine/ForecastFragment.java
+++ app/src/main/java/io/github/atimothee/sunshine/ForecastFragment.java
@@ -1,9 +1,11 @@
 package io.github.atimothee.sunshine;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -60,7 +62,9 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("kampala");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+            weatherTask.execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
