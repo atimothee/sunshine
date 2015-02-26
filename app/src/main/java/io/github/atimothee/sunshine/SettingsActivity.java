fbf43127a1a3d7c567159a9e9693e4bbfcbe85a0
Timothy Asiimwe <atimothee@gmail.com>
2015-02-27 00:45:57 +0300

    added preferences to settings activity

--- app/src/main/java/io/github/atimothee/sunshine/SettingsActivity.java
+++ app/src/main/java/io/github/atimothee/sunshine/SettingsActivity.java
@@ -1,9 +1,23 @@
 package io.github.atimothee.sunshine;
 
+import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 /**
  * Created by Timo on 2/27/15.
  */
-public class SettingsActivity extends PreferenceActivity{
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pre_general);
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
 }
