916492eb07db91ba81b95e0d294ae8d2804d164a
Timothy Asiimwe <atimothee@gmail.com>
2015-03-06 18:25:11 +0300

    insert read test passing for location table

--- app/src/androidTest/java/io/github/atimothee/sunshine/TestDb.java
+++ app/src/androidTest/java/io/github/atimothee/sunshine/TestDb.java
@@ -1,18 +1,65 @@
 package io.github.atimothee.sunshine;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
+import android.util.Log;
 
+import io.github.atimothee.sunshine.data.WeatherContract;
 import io.github.atimothee.sunshine.data.WeatherDbHelper;
+import io.github.atimothee.sunshine.data.WeatherContract.LocationEntry;
 
 /**
  * Created by Timo on 3/6/15.
  */
 public class TestDb extends AndroidTestCase{
+    private static final String LOG_TAG = TestDb.class.getSimpleName();
     public void testCreateDb() throws Throwable{
         mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
         SQLiteDatabase db = new WeatherDbHelper(this.mContext).getWritableDatabase();
         assertEquals(true, db.isOpen());
         db.close();
+    }
+
+    public void testInsertReadDb(){
+        String testName = "Kampala";
+        String testLocationSetting = "99705";
+        double testLatitude = 64.772;
+        double testLongitude = -147.355;
+        WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(LocationEntry.COLUMN_CITY_NAME, testName);
+        values.put(LocationEntry.COLUMN_LOCATION_SETTING, testLocationSetting);
+        values.put(LocationEntry.COLUMN_COORD_LAT, testLatitude);
+        values.put(LocationEntry.COLUMN_COORD_LONG, testLongitude);
+        long locationRowId;
+        locationRowId = db.insert(LocationEntry.TABLE_NAME, null, values);
+        assertTrue(locationRowId != -1);
+        Log.d(LOG_TAG, "New row id: "+ locationRowId);
+
+        String[] columns = {LocationEntry._ID, LocationEntry.COLUMN_LOCATION_SETTING, LocationEntry.COLUMN_COORD_LAT,
+                LocationEntry.COLUMN_COORD_LONG, LocationEntry.COLUMN_CITY_NAME};
+
+        Cursor cursor = db.query(LocationEntry.TABLE_NAME, columns, null, null, null, null, null);
+        if(cursor.moveToFirst()){
+            int locationIndex = cursor.getColumnIndex(LocationEntry.COLUMN_LOCATION_SETTING);
+            String location = cursor.getString(locationIndex);
+            int nameIndex = cursor.getColumnIndex(LocationEntry.COLUMN_CITY_NAME);
+            String name = cursor.getString(nameIndex);
+            int latIndex = cursor.getColumnIndex(LocationEntry.COLUMN_COORD_LAT);
+            double latitude = cursor.getDouble(latIndex);
+            int longIndex = cursor.getColumnIndex(LocationEntry.COLUMN_COORD_LONG);
+            double longitude = cursor.getDouble(longIndex);
+            assertEquals(testName, name);
+            assertEquals(testLocationSetting, location);
+            assertEquals(testLatitude, latitude);
+            assertEquals(testLongitude, longitude);
+        }
+        else {
+            fail("No values returned!");
+        }
+
     }
 }
