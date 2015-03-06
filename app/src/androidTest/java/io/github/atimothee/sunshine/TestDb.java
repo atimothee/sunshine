c73e7f013a8e6d66c0fa33886c509dbc29556396
Timothy Asiimwe <atimothee@gmail.com>
2015-03-06 19:25:11 +0300

    simplified test --failing

--- app/src/androidTest/java/io/github/atimothee/sunshine/TestDb.java
+++ app/src/androidTest/java/io/github/atimothee/sunshine/TestDb.java
@@ -6,6 +6,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
+import java.util.Map;
+import java.util.Set;
+
 import io.github.atimothee.sunshine.data.WeatherContract;
 import io.github.atimothee.sunshine.data.WeatherDbHelper;
 import io.github.atimothee.sunshine.data.WeatherContract.LocationEntry;
@@ -24,45 +27,53 @@ public class TestDb extends AndroidTestCase{
     }
 
     public void testInsertReadDb(){
-        String testName = "Kampala";
-        String testLocationSetting = "99705";
-        double testLatitude = 64.772;
-        double testLongitude = -147.355;
+
         WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put(LocationEntry.COLUMN_CITY_NAME, testName);
-        values.put(LocationEntry.COLUMN_LOCATION_SETTING, testLocationSetting);
-        values.put(LocationEntry.COLUMN_COORD_LAT, testLatitude);
-        values.put(LocationEntry.COLUMN_COORD_LONG, testLongitude);
+
         long locationRowId;
-        locationRowId = db.insert(LocationEntry.TABLE_NAME, null, values);
+        locationRowId = db.insert(LocationEntry.TABLE_NAME, null, getLocationContentValues());
         assertTrue(locationRowId != -1);
         Log.d(LOG_TAG, "New row id: "+ locationRowId);
-
-        String[] columns = {LocationEntry._ID, LocationEntry.COLUMN_LOCATION_SETTING, LocationEntry.COLUMN_COORD_LAT,
-                LocationEntry.COLUMN_COORD_LONG, LocationEntry.COLUMN_CITY_NAME};
-
-        Cursor cursor = db.query(LocationEntry.TABLE_NAME, columns, null, null, null, null, null);
+        Cursor cursor = db.query(LocationEntry.TABLE_NAME, null, null, null, null, null, null);
         if(cursor.moveToFirst()){
-            int locationIndex = cursor.getColumnIndex(LocationEntry.COLUMN_LOCATION_SETTING);
-            String location = cursor.getString(locationIndex);
-            int nameIndex = cursor.getColumnIndex(LocationEntry.COLUMN_CITY_NAME);
-            String name = cursor.getString(nameIndex);
-            int latIndex = cursor.getColumnIndex(LocationEntry.COLUMN_COORD_LAT);
-            double latitude = cursor.getDouble(latIndex);
-            int longIndex = cursor.getColumnIndex(LocationEntry.COLUMN_COORD_LONG);
-            double longitude = cursor.getDouble(longIndex);
-            assertEquals(testName, name);
-            assertEquals(testLocationSetting, location);
-            assertEquals(testLatitude, latitude);
-            assertEquals(testLongitude, longitude);
+            validateCursor(getLocationContentValues(), cursor);
         }
         else {
             fail("No values returned!");
         }
+
+
+
+        long weatherRowId = db.insert(WeatherEntry.TABLE_NAME, null, getWeatherContentValues(locationRowId));
+        assertTrue(weatherRowId!=-1);
+
+        Cursor weatherCursor = db.query(WeatherEntry.TABLE_NAME, null, null, null, null, null, null);
+        if(weatherCursor.moveToFirst()){
+            validateCursor(getWeatherContentValues(locationRowId), weatherCursor);
+
+        }
+        else {
+            fail("No weather values returned!");
+        }
+
+    }
+
+    static public void validateCursor(ContentValues expectedValues, Cursor valueCursor){
+        Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
+        for(Map.Entry<String, Object> entry: valueSet){
+            String columnName = entry.getKey();
+            int idx = valueCursor.getColumnIndex(columnName);
+            assertFalse(-1==idx);
+            Log.d(LOG_TAG, "column name: "+columnName);
+            String expectedValue = entry.getValue().toString();
+            assertEquals(expectedValue, valueCursor.getString(idx));
+        }
+    }
+
+    ContentValues getWeatherContentValues(long locationId){
         ContentValues weatherContentValues = new ContentValues();
-        weatherContentValues.put(WeatherEntry.COLUMN_LOC_KEY, locationRowId);
+        weatherContentValues.put(WeatherEntry.COLUMN_LOC_KEY, locationId);
         String testDate = "11/03/2015";
         weatherContentValues.put(WeatherEntry.COLUMN_DATETEXT, testDate);
         Double testDegrees = 1.1;
@@ -81,49 +92,19 @@ public class TestDb extends AndroidTestCase{
         weatherContentValues.put(WeatherEntry.COLUMN_WIND_SPEED, testWindSpeed);
         Integer testWeatherId = 243;
         weatherContentValues.put(WeatherEntry.COLUMN_WEATHER_ID, testWeatherId);
+        return weatherContentValues;
+    }
 
-        long weatherRowId = db.insert(WeatherEntry.TABLE_NAME, null, weatherContentValues);
-        assertTrue(weatherRowId!=-1);
-
-        String[] weatherColumns = {WeatherEntry.COLUMN_HUMIDITY, WeatherEntry.COLUMN_DATETEXT, WeatherEntry.COLUMN_LOC_KEY,
-        WeatherEntry.COLUMN_DEGREES, WeatherEntry.COLUMN_MAX_TEMP, WeatherEntry.COLUMN_MIN_TEMP, WeatherEntry.COLUMN_PRESSURE,
-        WeatherEntry.COLUMN_SHORT_DESC, WeatherEntry.COLUMN_WEATHER_ID, WeatherEntry.COLUMN_PRESSURE, WeatherEntry.COLUMN_WIND_SPEED};
-
-        Cursor weatherCursor = db.query(WeatherEntry.TABLE_NAME, weatherColumns, null, null, null, null, null);
-        if(weatherCursor.moveToFirst()){
-            int maxIndex = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_MAX_TEMP);
-            Double maxTemp = weatherCursor.getDouble(maxIndex);
-            int minIndex = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_MIN_TEMP);
-            int pressureIndex = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_PRESSURE);
-            int windIndex = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_WIND_SPEED);
-            int weatherIdIndex = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_WEATHER_ID);
-            int dateIndex = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_DATETEXT);
-            int degreesIndex = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_DEGREES);
-            int shortDescIndex = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_SHORT_DESC);
-            int humidityIndex = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_HUMIDITY);
-            int locIndex = weatherCursor.getColumnIndex(WeatherEntry.COLUMN_LOC_KEY);
-            assertEquals(maxTemp, testMaxTemp);
-            Double minTemp = weatherCursor.getDouble(minIndex);
-            assertEquals(minTemp, testMinTemp);
-            Double pressure = weatherCursor.getDouble(pressureIndex);
-            assertEquals(pressure, testPressure);
-            Double windSpeed = weatherCursor.getDouble(windIndex);
-            assertEquals(windSpeed, testWindSpeed);
-            Double humidity = weatherCursor.getDouble(humidityIndex);
-            assertEquals(humidity, testHumidity);
-            Double degrees = weatherCursor.getDouble(degreesIndex);
-            assertEquals(degrees, testDegrees);
-            String date= weatherCursor.getString(dateIndex);
-            assertEquals(date, testDate);
-            Integer weatherId = weatherCursor.getInt(weatherIdIndex);
-            assertEquals(weatherId, testWeatherId);
-            long locationId = weatherCursor.getLong(locIndex);
-            assertEquals(locationId, locationRowId);
-
-        }
-        else {
-            fail("No weather values returned!");
-        }
-
+    ContentValues getLocationContentValues() {
+        String testName = "Kampala";
+        String testLocationSetting = "99705";
+        double testLatitude = 64.772;
+        double testLongitude = -147.355;
+        ContentValues values = new ContentValues();
+        values.put(LocationEntry.COLUMN_CITY_NAME, testName);
+        values.put(LocationEntry.COLUMN_LOCATION_SETTING, testLocationSetting);
+        values.put(LocationEntry.COLUMN_COORD_LAT, testLatitude);
+        values.put(LocationEntry.COLUMN_COORD_LONG, testLongitude);
+        return values;
     }
 }
