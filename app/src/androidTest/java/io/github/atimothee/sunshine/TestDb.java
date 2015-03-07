463c35b3e99af8f12aa0b555300248bac599f0a8
Timothy Asiimwe <atimothee@gmail.com>
2015-03-07 11:34:53 +0300

    insert test failing

--- app/src/androidTest/java/io/github/atimothee/sunshine/TestDb.java
+++ app/src/androidTest/java/io/github/atimothee/sunshine/TestDb.java
@@ -19,92 +19,37 @@ import io.github.atimothee.sunshine.data.WeatherContract.WeatherEntry;
  */
 public class TestDb extends AndroidTestCase{
     private static final String LOG_TAG = TestDb.class.getSimpleName();
-    public void testCreateDb() throws Throwable{
-        mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
-        SQLiteDatabase db = new WeatherDbHelper(this.mContext).getWritableDatabase();
-        assertEquals(true, db.isOpen());
-        db.close();
-    }
 
-    public void testInsertReadDb(){
-
+    public void testCreateDb () throws Throwable{
         WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        long locationRowId;
-        locationRowId = db.insert(LocationEntry.TABLE_NAME, null, getLocationContentValues());
-        assertTrue(locationRowId != -1);
-        Log.d(LOG_TAG, "New row id: "+ locationRowId);
-        Cursor cursor = db.query(LocationEntry.TABLE_NAME, null, null, null, null, null, null);
-        if(cursor.moveToFirst()){
-            validateCursor(getLocationContentValues(), cursor);
-        }
-        else {
-            fail("No values returned!");
-        }
+        assertTrue(db.isOpen());
+        db.close();
+    }
 
-
-
-        long weatherRowId = db.insert(WeatherEntry.TABLE_NAME, null, getWeatherContentValues(locationRowId));
-        assertTrue(weatherRowId!=-1);
-
-        Cursor weatherCursor = db.query(WeatherEntry.TABLE_NAME, null, null, null, null, null, null);
-        if(weatherCursor.moveToFirst()){
-            validateCursor(getWeatherContentValues(locationRowId), weatherCursor);
-
-        }
-        else {
-            fail("No weather values returned!");
+    public void testInsertReadDb() throws Throwable{
+        ContentValues contentValues = new ContentValues();
+        String cityName = "Kampala";
+        contentValues.put(LocationEntry._ID, 1);
+        contentValues.put(LocationEntry.COLUMN_CITY_NAME, cityName);
+        Double longitude = 2.4;
+        contentValues.put(LocationEntry.COLUMN_COORD_LONG, longitude);
+        Double latitude = 2.7;
+        contentValues.put(LocationEntry.COLUMN_COORD_LAT, latitude);
+        String location = "424";
+        contentValues.put(LocationEntry.COLUMN_LOCATION_SETTING, location);
+        WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        try {
+            long locationRowId = db.insert(LocationEntry.TABLE_NAME, null, contentValues);
+            assertTrue(locationRowId != -1);
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.d(LOG_TAG, e.getMessage());
         }
 
     }
 
-    static public void validateCursor(ContentValues expectedValues, Cursor valueCursor){
-        Set<Map.Entry<String, Object>> valueSet = expectedValues.valueSet();
-        for(Map.Entry<String, Object> entry: valueSet){
-            String columnName = entry.getKey();
-            int idx = valueCursor.getColumnIndex(columnName);
-            assertFalse(-1==idx);
-            Log.d(LOG_TAG, "column name: "+columnName);
-            String expectedValue = entry.getValue().toString();
-            assertEquals(expectedValue, valueCursor.getString(idx));
-        }
-    }
 
-    ContentValues getWeatherContentValues(long locationId){
-        ContentValues weatherContentValues = new ContentValues();
-        weatherContentValues.put(WeatherEntry.COLUMN_LOC_KEY, locationId);
-        String testDate = "11/03/2015";
-        weatherContentValues.put(WeatherEntry.COLUMN_DATETEXT, testDate);
-        Double testDegrees = 1.1;
-        weatherContentValues.put(WeatherEntry.COLUMN_DEGREES, testDegrees);
-        Double testHumidity = 1.2;
-        weatherContentValues.put(WeatherEntry.COLUMN_HUMIDITY, testHumidity);
-        Double testMaxTemp = 20.0;
-        weatherContentValues.put(WeatherEntry.COLUMN_MAX_TEMP, testMaxTemp);
-        Double testMinTemp = 10.0;
-        weatherContentValues.put(WeatherEntry.COLUMN_MIN_TEMP, testMinTemp);
-        Double testPressure = 1.3;
-        weatherContentValues.put(WeatherEntry.COLUMN_PRESSURE, testPressure);
-        String testShortDesc = "Asteroids";
-        weatherContentValues.put(WeatherEntry.COLUMN_SHORT_DESC, testShortDesc);
-        Double testWindSpeed = 1.5;
-        weatherContentValues.put(WeatherEntry.COLUMN_WIND_SPEED, testWindSpeed);
-        Integer testWeatherId = 243;
-        weatherContentValues.put(WeatherEntry.COLUMN_WEATHER_ID, testWeatherId);
-        return weatherContentValues;
-    }
-
-    ContentValues getLocationContentValues() {
-        String testName = "Kampala";
-        String testLocationSetting = "99705";
-        double testLatitude = 64.772;
-        double testLongitude = -147.355;
-        ContentValues values = new ContentValues();
-        values.put(LocationEntry.COLUMN_CITY_NAME, testName);
-        values.put(LocationEntry.COLUMN_LOCATION_SETTING, testLocationSetting);
-        values.put(LocationEntry.COLUMN_COORD_LAT, testLatitude);
-        values.put(LocationEntry.COLUMN_COORD_LONG, testLongitude);
-        return values;
-    }
 }
--- app/src/main/java/io/github/atimothee/sunshine/data/WeatherContract.java
+++ app/src/main/java/io/github/atimothee/sunshine/data/WeatherContract.java
@@ -3,29 +3,28 @@ package io.github.atimothee.sunshine.data;
 import android.provider.BaseColumns;
 
 /**
- * Created by Timo on 3/6/15.
+ * Created by Timo on 3/7/15.
  */
 public class WeatherContract {
-    public static final class WeatherEntry implements BaseColumns{
-        public static final String TABLE_NAME = "weather";
-        public static final String COLUMN_LOC_KEY = "location_id";
-        public static final String COLUMN_DATETEXT = "date";
-        public static final String COLUMN_WEATHER_ID = "weather_id";
-        public static final String COLUMN_SHORT_DESC = "short_desc";
-        public static final String COLUMN_MIN_TEMP = "min";
-        public static final String COLUMN_MAX_TEMP = "max";
-        public static final String COLUMN_HUMIDITY = "humidity";
-        public static final String COLUMN_PRESSURE = "pressure";
-        public static final String COLUMN_WIND_SPEED = "wind";
-        public static final String COLUMN_DEGREES = "degrees";
 
+    public static class WeatherEntry implements BaseColumns {
+        public final static String TABLE_NAME = "weather";
+        public final static String COLUMN_MAX_TEMP = "max_temp";
+        public final static String COLUMN_MIN_TEMP = "min_temp";
+        public final static String COLUMN_PRESSURE = "pressure";
+        public final static String COLUMN_WIND_SPEED = "wind_speed";
+        public final static String COLUMN_WEATHER_ID = "weather_id";
+        public final static String COLUMN_LOC_KEY = "location";
+        public static final String COLUMN_DATETEXT = "date";
+        public static final String COLUMN_DEGREES = "degrees";
+        public static final String COLUMN_SHORT_DESC = "short_desc";
     }
 
-    public static final class LocationEntry implements BaseColumns{
-        public static final String TABLE_NAME = "location";
-        public static final String COLUMN_LOCATION_SETTING = "location_setting";
-        public static final String COLUMN_CITY_NAME = "city_name";
-        public static final String COLUMN_COORD_LAT = "coord_lat";
-        public static final String COLUMN_COORD_LONG = "coord_long";
+    public static class LocationEntry implements BaseColumns{
+        public final static String TABLE_NAME = "location";
+        public final static String COLUMN_CITY_NAME = "city_name";
+        public final static String COLUMN_LOCATION_SETTING = "location_setting";
+        public static final String COLUMN_COORD_LAT = "latitude";
+        public static final String COLUMN_COORD_LONG = "longitude";
     }
 }
