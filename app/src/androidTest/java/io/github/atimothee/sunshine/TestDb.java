d73d95f11891971e463db22b8eff0926ef43fe17
Timothy Asiimwe <atimothee@gmail.com>
2015-03-10 22:51:34 +0300

    end of lesson 4a --class version

--- app/src/androidTest/java/io/github/atimothee/sunshine/TestDb.java
+++ app/src/androidTest/java/io/github/atimothee/sunshine/TestDb.java
@@ -6,50 +6,78 @@ import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
-import java.util.Map;
-import java.util.Set;
-
-import io.github.atimothee.sunshine.data.WeatherContract;
+import io.github.atimothee.sunshine.data.WeatherContract.WeatherEntry;
 import io.github.atimothee.sunshine.data.WeatherDbHelper;
 import io.github.atimothee.sunshine.data.WeatherContract.LocationEntry;
-import io.github.atimothee.sunshine.data.WeatherContract.WeatherEntry;
 
 /**
  * Created by Timo on 3/6/15.
  */
 public class TestDb extends AndroidTestCase{
     private static final String LOG_TAG = TestDb.class.getSimpleName();
-
-    public void testCreateDb () throws Throwable{
-        WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        assertTrue(db.isOpen());
+    public void testCreateDb() throws Throwable{
+        mContext.deleteDatabase(WeatherDbHelper.DATABASE_NAME);
+        SQLiteDatabase db = new WeatherDbHelper(this.mContext).getWritableDatabase();
+        assertEquals(true, db.isOpen());
         db.close();
     }
 
-    public void testInsertReadDb() throws Throwable{
-        ContentValues contentValues = new ContentValues();
-        String cityName = "Kampala";
-        contentValues.put(LocationEntry._ID, 1);
-        contentValues.put(LocationEntry.COLUMN_CITY_NAME, cityName);
-        Double longitude = 2.4;
-        contentValues.put(LocationEntry.COLUMN_COORD_LONG, longitude);
-        Double latitude = 2.7;
-        contentValues.put(LocationEntry.COLUMN_COORD_LAT, latitude);
-        String location = "424";
-        contentValues.put(LocationEntry.COLUMN_LOCATION_SETTING, location);
+    public void testInsertReadDb(){
+        String testName = "Kampala";
+        String testLocationSetting = "99705";
+        double testLatitude = 64.772;
+        double testLongitude = -147.355;
         WeatherDbHelper dbHelper = new WeatherDbHelper(mContext);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
-        try {
-            long locationRowId = db.insert(LocationEntry.TABLE_NAME, null, contentValues);
-            assertTrue(locationRowId != -1);
-        }catch (Exception e){
-            e.printStackTrace();
-            Log.d(LOG_TAG, e.getMessage());
+        ContentValues values = new ContentValues();
+        values.put(LocationEntry.COLUMN_CITY_NAME, testName);
+        values.put(LocationEntry.COLUMN_LOCATION_SETTING, testLocationSetting);
+        values.put(LocationEntry.COLUMN_COORD_LAT, testLatitude);
+        values.put(LocationEntry.COLUMN_COORD_LONG, testLongitude);
+        long locationRowId;
+        locationRowId = db.insert(LocationEntry.TABLE_NAME, null, values);
+        assertTrue(locationRowId != -1);
+        String[] columns = {LocationEntry.COLUMN_CITY_NAME, LocationEntry.COLUMN_COORD_LONG,
+        LocationEntry.COLUMN_COORD_LAT, LocationEntry.COLUMN_LOCATION_SETTING};
+        Cursor cursor = db.query(LocationEntry.TABLE_NAME, columns, null, null, null, null, null);
+        if(cursor.moveToFirst()){
+            int latIndex = cursor.getColumnIndex(LocationEntry.COLUMN_COORD_LAT);
+            assertEquals(testLatitude, cursor.getDouble(latIndex));
+            int longIndex = cursor.getColumnIndex(LocationEntry.COLUMN_COORD_LONG);
+            assertEquals(testLongitude, cursor.getDouble(longIndex));
         }
 
+        ContentValues weatherContentValues = new ContentValues();
+        int testWeatherId = 345;
+        weatherContentValues.put(WeatherEntry.COLUMN_WEATHER_ID, testWeatherId);
+        Double testPressure = 3.5;
+        weatherContentValues.put(WeatherEntry.COLUMN_PRESSURE, testPressure);
+        Double testMaxTemp = 40.0;
+        weatherContentValues.put(WeatherEntry.COLUMN_MAX_TEMP, testMaxTemp);
+        Double testMinTemp = 14.0;
+        weatherContentValues.put(WeatherEntry.COLUMN_MIN_TEMP, testMinTemp);
+        weatherContentValues.put(WeatherEntry.COLUMN_LOC_KEY, locationRowId);
+        String testDesc = "Cloudy";
+        weatherContentValues.put(WeatherEntry.COLUMN_SHORT_DESC, testDesc);
+        String testDate = "07/03/2015";
+        weatherContentValues.put(WeatherEntry.COLUMN_DATETEXT, testDate);
+        Double testHumidity =4.5;
+        weatherContentValues.put(WeatherEntry.COLUMN_HUMIDITY, testHumidity);
+        Double testDegrees =30.0;
+        weatherContentValues.put(WeatherEntry.COLUMN_DEGREES, testDegrees);
+        Double windSpeed = 400.0;
+        weatherContentValues.put(WeatherEntry.COLUMN_WIND_SPEED, windSpeed);
+
+        long weatherRowId = db.insert(WeatherEntry.TABLE_NAME, null, weatherContentValues);
+        assertTrue(weatherRowId!=-1);
+        Cursor cursor1 = db.query(WeatherEntry.TABLE_NAME, null, null, null, null, null, null);
+        if(cursor1.moveToFirst()){
+            int humidityIndex = cursor1.getColumnIndex(WeatherEntry.COLUMN_HUMIDITY);
+            assertEquals(testHumidity, cursor1.getDouble(humidityIndex));
+            int locationIndex  =cursor1.getColumnIndex(WeatherEntry.COLUMN_LOC_KEY);
+            assertEquals(locationRowId, cursor1.getLong(locationIndex));
+        }
+
+
     }
-
-
 }
--- app/src/main/java/io/github/atimothee/sunshine/data/WeatherDbHelper.java
+++ app/src/main/java/io/github/atimothee/sunshine/data/WeatherDbHelper.java
@@ -7,19 +7,20 @@ import io.github.atimothee.sunshine.data.WeatherContract.LocationEntry;
 import io.github.atimothee.sunshine.data.WeatherContract.WeatherEntry;
 
 /**
- * Created by Timo on 3/7/15.
+ * Created by Timo on 3/6/15.
  */
 public class WeatherDbHelper extends SQLiteOpenHelper{
+    private static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "weather.db";
-    public static final int DATABASE_VERSION = 1;
-
     public WeatherDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        final String SQL_CREATE_LOCATION_TABLE = LocationEntry._ID+" INTEGER PRIMARY KEY, "+
+
+        final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE "+LocationEntry.TABLE_NAME+ "("+
+                LocationEntry._ID+" INTEGER PRIMARY KEY, "+
                 LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL, "+
                 LocationEntry.COLUMN_CITY_NAME + " TEXT NOT NULL, "+
                 LocationEntry.COLUMN_COORD_LAT + " REAL NOT NULL, "+
@@ -27,13 +28,15 @@ public class WeatherDbHelper extends SQLiteOpenHelper{
                 " UNIQUE("+LocationEntry.COLUMN_LOCATION_SETTING + ") ON CONFLICT IGNORE"+
                 " );";
 
-        final String SQL_CREATE_WEATHER_TABLE = WeatherEntry._ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+
+        final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE "+ WeatherEntry.TABLE_NAME + "(" +
+                WeatherEntry._ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+
                 WeatherEntry.COLUMN_LOC_KEY+" INTEGER NOT NULL, "+
                 WeatherEntry.COLUMN_DATETEXT+" TEXT NOT NULL, "+
                 WeatherEntry.COLUMN_SHORT_DESC+" TEXT NOT NULL, "+
                 WeatherEntry.COLUMN_WEATHER_ID+" INTEGER NOT NULL, "+
                 WeatherEntry.COLUMN_MIN_TEMP+" REAL NOT NULL, "+
                 WeatherEntry.COLUMN_MAX_TEMP+" REAL NOT NULL, "+
+                WeatherEntry.COLUMN_HUMIDITY+" REAL NOT NULL, "+
                 WeatherEntry.COLUMN_PRESSURE+" REAL NOT NULL, "+
                 WeatherEntry.COLUMN_WIND_SPEED+" REAL NOT NULL, "+
                 WeatherEntry.COLUMN_DEGREES+" REAL NOT NULL, "+
@@ -45,19 +48,12 @@ public class WeatherDbHelper extends SQLiteOpenHelper{
         db.execSQL(SQL_CREATE_LOCATION_TABLE);
         db.execSQL(SQL_CREATE_WEATHER_TABLE);
 
-
-        db.execSQL("DROP DATABASE IF EXISTS "+ LocationEntry.TABLE_NAME);
-        db.execSQL("DROP DATABASE IF EXISTS "+ WeatherEntry.TABLE_NAME);
-        onCreate(db);
-        db.execSQL(SQL_CREATE_LOCATION_TABLE);
-        db.execSQL(SQL_CREATE_WEATHER_TABLE);
-
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-        db.execSQL(" DROP TABLE IF EXISTS "+LocationEntry.TABLE_NAME);
-        db.execSQL(" DROP TABLE IF EXISTS "+WeatherEntry.TABLE_NAME);
+        db.execSQL("DROP DATABASE IF EXISTS "+ LocationEntry.TABLE_NAME);
+        db.execSQL("DROP DATABASE IF EXISTS "+ WeatherEntry.TABLE_NAME);
+        onCreate(db);
     }
 }
